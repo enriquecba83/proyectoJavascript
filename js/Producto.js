61737e1b20e43ca32eedf680eecda0b8e6882b0c
Enrique Cativa <cativa@bancojuliosigb.com>
2023-04-28 15:30:32 -0300

    se agrega sass

---
+++ js/Producto.js
@@ -0,0 +1,36 @@
+class Producto{
+    //atributos
+    id;
+    nombreProducto;
+    descripcion;
+    rubro;
+    cantProducto;
+    imagenProducto;
+    precio;
+
+    //constructor
+    constructor(id,nombreProducto,descripcion,rubro,precio)
+    {
+        this.id             = id;
+        this.nombreProducto = nombreProducto;
+        this.descripcion    = descripcion;
+        this.rubro          = rubro;
+        this.precio         = precio;
+    }
+
+    //metodos
+    agregar = function()
+    {
+
+    }
+
+    quitar = function()
+    {
+
+    }
+
+    modificar = function()
+    {
+        
+    }
+}
